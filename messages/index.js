660e567845251782a9abee0e6f48ce4f515bdf98
fanzWahr <francois-p@hotmail.de>
2017-06-20 17:47:12 +0200

    some changes to functionality, i.e. some more questions.

--- messages/index.js
+++ messages/index.js
@@ -22,33 +22,44 @@ bot.localePath(path.join(__dirname, './locale'));
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hello you!... What's your name?");
+        builder.Prompts.text(session, "Hello!... What's your name?");
     },
     function (session, results) {
         session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ". I'd hope you would like to learn something about building bots? How many years have you been coding?"); 
+        builder.Prompts.number(session, "Hi " + results.response + ". I hope you would like to learn something about building bots? How many years have you been coding?"); 
     },
     function (session, results) {
         session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+        builder.Prompts.choice(session, "What language have you used for coding before?", ["Java", "JavaScript", "TypeScript", "C#"]);
     },
     function (session, results) {
         session.userData.language = results.response.entity;
         session.send("Got it... " + session.userData.name + 
                     " you've been programming for " + session.userData.coding + 
                     " years and use " + session.userData.language + ".");
-        builder.Prompts.choice(session, "Now do you want to learn something about building bots for educational pruposes?", ["yes", "no"]);
+        builder.Prompts.choice(session, "Now do you want to learn something about building bots?", ["yes", "no"]);
     },    
     function (session, results) {
         session.userData.willToLearn = results.response.entity;
         
         if(session.userData.willToLearn=="yes"){
-        session.send("Got it... so here is a first definition: a chatbot for educational purposes is chatbot that helps you to gain knowledge about a specific topic.");
+        session.send("Got it... so here is a first definition: a chatbot is a program and a communication medium, which isn't necessarily linked to AI but always has to be able of processing natural language in textual or spoken form, and normally should provide some benefit. I - as chatbot - am trying to help you to gain knowledge about a specific topic, i.e.: Building and using chatbots for educational purposes.");
+        builder.Prompts.choice(session, "Now you want to learn something about building bots... bot for which purpose?", ["educational", "business"]);
         }
-        else {
+       /* else {
             session.send("Got it, you don't want to learn");
-        }
+        }*/
     },
+    function (session, results) {
+        session.userData.purpose = results.response.entity;
+        
+        if(session.userData.purpose=="educational"){
+            session.send("That's great! An educational chatbot... ummmh well, i don't really know a lot about that.")//TODO
+        }
+        if(session.userData.purpose=="business"){
+            session.send("That's great! Bot I won't be really able to help you. Maybe you could have a look on this fancy website : https://chatbotsmagazine.com/whats-it-like-to-build-run-a-chatbot-business-751935913300 ... and also IBM Watson might be interesting for you! ")
+        }
+    }   
 ]);
 
 if (useEmulator) {
