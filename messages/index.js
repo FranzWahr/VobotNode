1344f504a124e89debd0c336ff52be0b81c50358
fanzWahr <francois-p@hotmail.de>
2017-06-20 17:20:46 +0200

    some changes to functionality, i.e. some more questions.

--- messages/index.js
+++ messages/index.js
@@ -37,10 +37,8 @@ bot.dialog('/', [
         session.send("Got it... " + session.userData.name + 
                     " you've been programming for " + session.userData.coding + 
                     " years and use " + session.userData.language + ".");
-    },
-    function (session) {
         builder.Prompts.choice(session, "Now do you want to learn something about building bots for educational pruposes?", ["yes", "no"]);
-    },
+    },    
     function (session, results) {
         session.userData.willToLearn = results.response.entity;
         
