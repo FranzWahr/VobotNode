8dac526b939461afcc269dbbe0ba056e17cc8d83
fanzWahr <francois-p@hotmail.de>
2017-06-22 11:20:33 +0200

    new feature fix

--- messages/index.js
+++ messages/index.js
@@ -22,7 +22,7 @@ bot.localePath(path.join(__dirname, './locale'));
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hello!... What's your name?");
+        builder.Prompts.text(session, "HeyHo, Hello!... What's your name?");
     },
     function (session, results) {
         session.userData.name = results.response;
@@ -69,17 +69,16 @@ bot.dialog('/', [
         else{
             session.send("No that's only your opinion ...that was not correct. It is b)! If you want to know why you could check this site: https://docs.microsoft.com/en-us/bot-framework/bot-design-principles")
         }
-        session.send("Give me the three most important concepts of chatbot!");
      },
-/*     function(session, results) {
+     function(session, results) {
      	session.userData.definitionAnswer = results.response;
-     	var string definition1 = "medium";
-     	var string definition2 = "program";
-     	var string definition3 = "natural language processing";
-     	var int rating = 0;
-     	if (results.response.indexOf(definition1 !== -1) rating++);
-     	if (results.response.indexOf(definition2 !== -1) rating++);
-     	if (results.response.indexOf(definition3 !== -1) rating++);
+     	var definition1 = "medium";
+     	var definition2 = "program";
+     	var definition3 = "natural language processing";
+     	var rating = 0;
+     	if (results.response.indexOf(definition1 !== -1)) rating++;
+     	if (results.response.indexOf(definition2 !== -1)) rating++;
+     	if (results.response.indexOf(definition3 !== -1)) rating++;
      	if(result == 3){
      		session.send("That's correct! There's not much I can still teach you!")
      	}
@@ -89,8 +88,7 @@ bot.dialog('/', [
      	else {
      		session.send("Uhm, unfortunately that was all wrong...")
 		}
-     }, */
-
+     }, 
 ]);
 
 if (useEmulator) {
